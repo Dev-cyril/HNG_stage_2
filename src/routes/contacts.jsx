812cbcdc4bed627c766f5e8bb2e6e599120b00b1
Cyril <cyril.idu2@gmail.com>
2022-11-07 11:26:36 +0100

    feat: added a required attribute to the form fields style: gave the input boxes a padding

--- src/routes/contacts.jsx
+++ src/routes/contacts.jsx
@@ -12,26 +12,25 @@ const ContactMe = () => {
                 <h2>Contact Me</h2>
                 <p>Hi there, contact me to ask me about anything you have in mind.</p>
                 <form action="">
-                    <label id='f1'>
+                    <label id='f1' for='first name'>
                         First Name <br />
                         <input id='first_name' type="text" placeholder="Enter your first name"/>
                     </label>
-                    <label id='f2'>
+                    <label id='f2' for='last name'>
                         Last Name <br />
                         <input id='last_name' type="text" placeholder="Enter your last name"/>
                     </label>
-                    <label id='f3'>
+                    <label id='f3' for='Email'>
                         Email <br />
                         <input id='email' type="email" placeholder="yourname@email.com"/>
                     </label>
-                    <label id='f4'>
+                    <label id='f4' for='Message'>
                         Message <br />
-                        <textarea id='message' type="text" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+                        <textarea id='message' type="text" placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
                     </label>
                     <label id='f5'>
                         <input type='checkbox' />
-                        <span id='check'></span>
-                        <h5>You agree to providing your data to {name} who may contact you.</h5>
+                        <h5>You agree to providing your data to Cyril who may contact you.</h5>
                     </label>
                     <button id='btn_submit'>Send message</button>
                 </form>
